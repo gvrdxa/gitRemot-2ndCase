f1ace6f2e1ca183b1ffe42e6938fd6fa4675c6cc
gvrdxa <gvarddien19@gmail.com>
2024-11-17 07:03:17 +0900

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
     </head>
 <body>
     <h1>2nd Case</h1>
+    <p>Lorem ipsum dolor sit 
+        amet consectetur adipisicing elit. 
+        Provident dignissimos sit rem et. 
+        Delectus eius harum molestiae. 
+        Cum, unde at, veritatis porro non
+        ipsa excepturi quas sapiente eum ea eius.
+    </p>
 </body>
 </html>
